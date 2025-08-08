86803405e4bf15b5ce123140b483104761f0bdb1
Theolin Nadasen <nadasentheolin@gmail.com>
2025-08-08 03:21:58 +0200

    added base 64 encoders and bind function

--- langchain_helper.py
+++ langchain_helper.py
@@ -2,7 +2,7 @@ import base64
 from PIL import Image
 from io import BytesIO
 
-def convert_to_base64(pil_image):
+def jpg_to_base64(pil_image):
     """
     Convert PIL images to Base64 encoded strings
 
@@ -14,3 +14,20 @@ def convert_to_base64(pil_image):
     pil_image.save(buffered, format="JPEG")  # You can change the format if needed
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
     return img_str
+
+def png_to_base64(pil_image):
+    """
+    Convert PIL images to Base64 encoded strings
+
+    :param pil_image: PIL image
+    :return: Re-sized Base64 string
+    """
+
+    buffered = BytesIO()
+    pil_image.save(buffered, format="PNG")  # You can change the format if needed
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+def bind_image_to_llm(image_b64, llm):
+    llm_with_image = llm.bind(images=[image_b64])
+    return llm_with_image
